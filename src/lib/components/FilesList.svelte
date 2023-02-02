fba9fd2b79ee38d2304e530511da5fd3b34ec2bb
Miguel Langarano <miguelangarano@gmail.com>
2023-02-02 01:27:23 -0500

    files upload and list, contract sign

---
+++ src/lib/components/FilesList.svelte
@@ -0,0 +1,53 @@
+<script>
+    import { format } from "date-fns";
+    export let files;
+
+
+    function openFile(url){
+        window.open(url, "_blank");
+    }
+</script>
+
+<div class="flex w-full items-center justify-center">
+    <div class="overflow-x-auto">
+        <table class="table w-full">
+            <!-- head -->
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Description</th>
+                    <th>Type</th>
+                    <th>Time</th>
+                    <th>Uploader</th>
+                    <th>Link</th>
+                </tr>
+            </thead>
+            <tbody>
+                {#each files as { fileDescription, fileHash, fileId, fileName, fileType, uploadTime, uploader }}
+                    <tr>
+                        <th>{fileId}</th>
+                        <td>{fileName}</td>
+                        <td>{fileDescription}</td>
+                        <td>{fileType}</td>
+                        <td
+                            >{format(
+                                new Date(uploadTime * 1000),
+                                "MMM Do 'of' yyyy 'at' HH:mm"
+                            )}</td
+                        >
+                        <td>{uploader}</td>
+                        <td>
+                            <button
+                                class="btn bg-teal-600"
+                                on:click={()=>openFile(`https://condor.infura-ipfs.io/ipfs/${fileHash}`)}
+                            >
+                                Open
+                            </button>
+                        </td>
+                    </tr>
+                {/each}
+            </tbody>
+        </table>
+    </div>
+</div>
